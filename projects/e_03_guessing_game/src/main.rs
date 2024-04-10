b60a1001311144185ab5ff9f12087bdbab51f938
eduairet <hola@eduairet.com>
2024-04-09 20:42:49 -0600

    Guess game

---
+++ projects/e_03_guessing_game/src/main.rs
@@ -0,0 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100); // This generates a random number between 1 and 100
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        }; // Here we'll ask the user to input a number, if the input is not a number, we'll continue the loop
+
+        println!("You guessed: {}", guess);
+
+        // match is a control flow operator that compares a value to a series of patterns and then executes code based on which pattern matches
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
