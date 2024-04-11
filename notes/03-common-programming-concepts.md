c8c9d1217a58cbb352263b87f1b9a95b99f35b8e
eduairet <hola@eduairet.com>
2024-04-10 19:15:19 -0600

    Scalar types

--- notes/03-common-programming-concepts.md
+++ notes/03-common-programming-concepts.md
@@ -1,1 +1,64 @@
 # Common Programming Concepts
+
+## Varialbes and Mutability
+
+- Variables are immutable by default
+- Use `let` to declare a variable
+- Use `mut` to make a variable mutable
+- Constants are always immutable and must be annotated with a type
+  - Use `const` to declare a constant
+  - Constants can be declared in any scope, including the global scope
+  - Constant naming convention is to use all uppercase with underscores between words (`SOME_CONSTANT`)
+- Shadowing allows to reuse a variable name
+  - This will make the compiler to use the shadowed variable instead of the original one
+  - Shadowing is useful to change the type of a variable without changing its name
+
+## Data Types
+
+- Rust is a statically hard typed language
+- Rust must know the types of all variables at compile time, but the compiler can usually infer the types
+- Rust has two data type categories: scalar (single value) and compound (multiple values)
+
+### Scalar Types
+
+- Numbers:
+
+  - Integers:
+    | Length | Signed | Unsigned |
+    | ------ | ------ | -------- |
+    | 8-bit | `i8` | `u8` |
+    | 16-bit | `i16` | `u16` |
+    | 32-bit | `i32` | `u32` |
+    | 64-bit | `i64` | `u64` |
+    | 128-bit| `i128` | `u128` |
+    | arch | `isize`| `usize` |
+    - The default integer type is `i32`
+    - Signed integers can store positive and negative numbers
+    - Unsigned integers can store only positive numbers
+    - `arch` depends on the kind of computer your program is running on
+    - Numbers can use underscores to improve readability: `let x: i32 = 1_000;`
+    - You can use type suffixes to specify the type of a number: `let x = 42u32;`
+  - Integer literals:
+    - Decimal: `98_222`
+    - Hex: `0xff`
+    - Octal: `0o77`
+    - Binary: `0b1111_0000`
+    - Byte (u8 only): `b'A'`
+  - Floating-point numbers: `f32`, `f64`
+    - Numbers with a decimal point
+    - Default type is `f64` because it's roughly the same speed as `f32` but is more precise
+    - Rust uses the IEEE-754 standard for floating-point numbers
+  - Rust supports basic mathematical operations: `+`, `-`, `*`, `/`, `%`
+
+- Booleans: `bool` (`true`, `false`)
+- Characters: `char` (single Unicode character, 4 bytes)
+  - `'a' '😻'`
+
+### Compound types
+
+- They can group multiple values into one type
+
+## Appendix
+
+- Reserved keywords: [Rust Reference](https://doc.rust-lang.org/reference/keywords.html)
+- Allowed constant evaluations: [Rust Reference](https://doc.rust-lang.org/reference/const_eval.html)
---
+++ projects/e_03_common_programming_concepts/src/main.rs
@@ -0,0 +1,70 @@
+fn main() {
+    let x = 5;
+    println!("The value of x is: {}", x);
+    /*
+    // If we try to change the value of x, we will get a compile-time error
+    x = 6;
+    ```
+    3 |     println!("The value of x is: {}", x);
+    4 |     x = 6;
+      |     ^^^^^ cannot assign twice to immutable variable
+    ```
+    */
+    let mut y = 10;
+    println!("The value of y is: {}", y);
+    y = 6;
+    println!("The value of y is: {}", y);
+    /*
+    Here we can see that we can change the value of y because it is mutable.
+    The value of y is: 5
+    The value of y is: 6
+    */
+    const LANGUAGE: &str = "Rust";
+    const DAYS_OF_TWO_WEEKS: u32 = 7 * 2; // Constants can be assigned expressions
+
+    let shadowed_variable = DAYS_OF_TWO_WEEKS;
+    {
+        // You can shadow on different scopes
+        let shadowed_variable = LANGUAGE;
+        println!(
+            "The value of inner scope shadowed_variable is: {}",
+            shadowed_variable
+        );
+    }
+    println!("The value of shadowed_variable is: {}", shadowed_variable);
+    let shadowed_variable = LANGUAGE; // we can change the type of the variable when shadowing
+    println!("The value of shadowed_variable is: {}", shadowed_variable);
+
+    // Scalar types
+
+    // Integers, signed | -2^(bit - 1) to 2^(bit - 1) - 1
+    let scalar_integer_i8: i8 = -128; // 2^7
+    let scalar_integer_i16: i16 = -32768; // 2^15
+    let scalar_integer_i32: i32 = -2147483648; // 2^31
+    let scalar_integer_i64: i64 = -9223372036854775808; // 2^63
+    let scalar_integer_i128: i128 = -170141183460469231731687303715884105728; // 2^127
+    let scalar_integer_isize: isize = -9223372036854775808; // 2^63
+
+    // Integers, unsigned | 2^bit - 1
+    let scalar_integer_u8: u8 = 255; // 2^8 - 1
+    let scalar_integer_u16: u16 = 65535; // 2^16 - 1
+    let scalar_integer_u32: u32 = 4294967295; // 2^32 - 1
+    let scalar_integer_u64: u64 = 18446744073709551615; // 2^64 - 1
+    let scalar_integer_u128: u128 = 340282366920938463463374607431768211455; // 2^128 - 1
+    let scalar_integer_usize: usize = 18446744073709551615; // 2^64 - 1
+
+    // Floating point
+    let scalar_float_f32: f32 = 3.141516; // 32-bit floating point
+    let scalar_float_f64: f64 = 3.14159265358979323; // 64-bit floating point - More precise
+
+    // Numeric operations
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let truncated = 43 / 5; // Truncates the result to the nearest integer
+    let remainder = 43 % 5;
+
+    let scalar_boolean: bool = true;
+    let scalar_character: char = 'a';
+}
